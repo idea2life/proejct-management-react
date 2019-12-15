3719ef809db1ce922bf29504915598e865169973
hamleta83 <hamleta83@gmail.com>
2019-12-15 11:49:27 +0400

    some fix

--- src/views/Plan/index.js
+++ src/views/Plan/index.js
@@ -1,93 +1,14 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
-import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import classNames from 'classnames';
 import styles from './plan.scss';
-import ToggleButton from '@material-ui/lab/ToggleButton';
-import ToggleButtonGroup from '@material-ui/lab/ToggleButtonGroup';
 import TextField from '@material-ui/core/TextField';
 import TelegramIcon from '@material-ui/icons/Telegram';
 import FlightIcon from '@material-ui/icons/Flight';
 import VoicemailIcon from '@material-ui/icons/Voicemail';
 import AirplanemodeActiveIcon from '@material-ui/icons/AirplanemodeActive';
-import Slider from '@material-ui/core/Slider';
-
-const CssToggleButton = withStyles({
-    root: {
-        "&:focus": {
-            background: '#259fc4',
-            color: '#ffffff'
-        },
-        "& button.Mui-selected": {
-            background: '#259fc4',
-            color: '#ffffff'
-        }
-    },
-  })(ToggleButton);
-
-  const iOSBoxShadow =
-  '0 3px 1px rgba(0,0,0,0.1),0 4px 8px rgba(0,0,0,0.13),0 0 0 1px rgba(0,0,0,0.02)';
-  const marks = [
-    {
-      value: 0,
-    },
-    {
-      value: 1,
-    },
-  ];
-
-  const IOSSlider = withStyles({
-    root: {
-      color: '#3880ff',
-      height: 2,
-      padding: '15px 0',
-    },
-    thumb: {
-      height: 28,
-      width: 28,
-      backgroundColor: '#fff',
-      boxShadow: iOSBoxShadow,
-      marginTop: -14,
-      marginLeft: -14,
-      '&:focus,&:hover,&$active': {
-        boxShadow: '0 3px 1px rgba(0,0,0,0.1),0 4px 8px rgba(0,0,0,0.3),0 0 0 1px rgba(0,0,0,0.02)',
-        // Reset on touch devices, it doesn't add specificity
-        '@media (hover: none)': {
-          boxShadow: iOSBoxShadow,
-        },
-      },
-    },
-    active: {},
-    valueLabel: {
-      left: 'calc(-50% + 11px)',
-      top: -22,
-      '& *': {
-        background: 'transparent',
-        color: '#000',
-      },
-    },
-    track: {
-      height: 2,
-    },
-    rail: {
-      height: 2,
-      opacity: 0.5,
-      backgroundColor: '#bfbfbf',
-    },
-    mark: {
-      backgroundColor: '#bfbfbf',
-      height: 8,
-      width: 1,
-      marginTop: -3,
-    },
-    markActive: {
-      opacity: 1,
-      backgroundColor: 'currentColor',
-    },
-  })(Slider);
-  
 
 export default class Home extends React.Component {
     constructor(props) {
