c623759b3276915ad3d12767be5b27fa8edd88f5
hamleta83 <hamleta83@gmail.com>
2019-12-18 01:27:58 +0400

    update timeline chart

--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -55,61 +55,49 @@ export default function Dashboard() {
       id: 1,
       group: 1,
       title: '',
+      type: 'phase',
       start_time: moment(new Date(2019, 7, 29, 12)),
       end_time: moment(new Date(2019, 8, 10)),
-      itemProps: {
-        className: classes.phase,
-      }
     },
     {
       id: 2,
       group: 2,
       title: '',
+      type: 'level',
       start_time: moment(new Date(2019, 7, 29, 12)),
       end_time: moment(new Date(2019, 8, 10)),
-      itemProps: {
-        className: classes.level,
-      }
     },
     {
       id: 3,
       group: 3,
       title: '',
+      type: 'area',
       start_time: moment(new Date(2019, 7, 29, 12)),
       end_time: moment(new Date(2019, 8, 10)),
-      itemProps: {
-        className: classes.area,
-      }
     },
     {
       id: 4,
       group: 4,
       title: '',
+      type: 'task',
       start_time: moment(new Date(2019, 7, 29, 12)),
       end_time: moment(new Date(2019, 8, 2, 22)),
-      itemProps: {
-        className: classes.task,
-      }
     },
     {
       id: 5,
       group: 5,
       title: '',
+      type: 'task',
       start_time: moment(new Date(2019, 8, 3)),
       end_time: moment(new Date(2019, 8, 7, 5)),
-      itemProps: {
-        className: classes.task,
-      }
     },
     {
       id: 6,
       group: 6,
       title: '',
+      type: 'task',
       start_time: moment(new Date(2019, 8, 6, 2)),
       end_time: moment(new Date(2019, 8, 10)),
-      itemProps: {
-        className: classes.task,
-      }
     },
   ]
 
@@ -173,6 +161,61 @@ export default function Dashboard() {
               itemHeightRatio={0.17}
               defaultTimeStart={moment(new Date(2019, 7, 29))}
               defaultTimeEnd={moment(new Date(2019, 8, 10))}
+              itemRenderer = {({
+                item,
+                itemContext,
+                getItemProps,
+                getResizeProps,
+              }) => {
+                const { left: leftResizeProps, right: rightResizeProps } = getResizeProps()
+                const index = items.findIndex(element => element.id === item.id);
+                const nextItem = items[index + 1];
+                const delta = parseFloat(itemContext.width) / (item.end_time - item.start_time);
+                const deltaWidth = nextItem ? delta * (nextItem.start_time - item.end_time) : 0;
+                return (
+                  <div
+                    {...getItemProps(item.itemProps)}
+                    className={classNames([getItemProps(item.itemProps).className, classes[item.type]])}
+                  >
+                  {
+                    item.type === 'task' && nextItem && nextItem.start_time &&
+                    <div
+                      style={{
+                        position: 'absolute',
+                        left: (parseFloat(itemContext.width) - 5),
+                        top: 5,
+                        backgroundColor: '#707070',
+                        height: 30,
+                        width: 3
+                      }}
+                    />
+                  }
+                  {
+                    item.type === 'task' && nextItem && nextItem.type === 'task' && nextItem.start_time && new Date(nextItem.start_time) > new Date(item.end_time) &&
+                    <div
+                      style={{
+                        position: 'absolute',
+                        left: (parseFloat(itemContext.width) - 5),
+                        top: 35,
+                        backgroundColor: '#707070',
+                        height: 3,
+                        width: deltaWidth + 5
+                      }}
+                    />
+                  }
+                    {itemContext.useResizeHandle ? <div {...leftResizeProps} /> : ''}
+                    <div
+                      className="rct-item-content"
+                      style={{ maxHeight: `${itemContext.dimensions.height}` }}
+                    >
+                      {itemContext.title}
+                    </div>
+              
+                    {itemContext.useResizeHandle ? <div {...rightResizeProps} /> : ''}
+                  </div>
+                )}
+              
+              }
             >
               <TimelineHeaders>
                 <DateHeader unit="primaryHeader"
