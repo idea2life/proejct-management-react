15fde4e99deab2b14dd28a94b499635e81caeee0
hamleta83 <hamleta83@gmail.com>
2019-12-18 12:15:15 +0400

    convert schedule from function to class

--- src/views/Schedule/Schedule.js
+++ src/views/Schedule/Schedule.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
 import classNames from "classnames";
 // core components
 import GridItem from "components/Grid/GridItem.js";
@@ -18,151 +17,158 @@ import SaveOutlinedIcon from '@material-ui/icons/SaveOutlined';
 import MoreVertOutlinedIcon from '@material-ui/icons/MoreVertOutlined';
 
 import moment from 'moment'
-const useStyles = makeStyles(styles);
 
-export default function Schedule() {
-  const classes = useStyles();
-  const icons = [
-    { icon: CenterFocusWeakIcon },
-    { icon: ZoomOutIcon },
-    { icon: ZoomInIcon },
-    { icon: SaveOutlinedIcon },
-    { icon: ZoomOutMapIcon },
-    { icon: MoreVertOutlinedIcon }
-  ]
+export default class Schedule extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        userName: '',
+        password: ''
+    }
+  }
   
-  const groups = [
-    { id: 1, title: '' },
-    { id: 2, title: '' },
-    { id: 3, title: '' },
-    { id: 4, title: '' },
-    { id: 5, title: '' },
-    { id: 6, title: '' },
-    { id: 7, title: '' },
-    { id: 8, title: '' },
-    { id: 9, title: '' },
-    { id: 10, title: '' },
-    { id: 11, title: '' },
-    { id: 12, title: '' },
-    { id: 13, title: '' }
-  ]
-  
-  const items = [
-    {
-      id: 1,
-      group: 1,
-      title: '',
-      type: 'phase',
-      start_time: moment(new Date(2019, 7, 29, 12)),
-      end_time: moment(new Date(2019, 8, 10)),
-    },
-    {
-      id: 2,
-      group: 2,
-      title: '',
-      type: 'level',
-      start_time: moment(new Date(2019, 7, 29, 12)),
-      end_time: moment(new Date(2019, 8, 10)),
-    },
-    {
-      id: 3,
-      group: 3,
-      title: '',
-      type: 'area',
-      start_time: moment(new Date(2019, 7, 29, 12)),
-      end_time: moment(new Date(2019, 8, 10)),
-    },
-    {
-      id: 4,
-      group: 4,
-      title: '',
-      type: 'task',
-      start_time: moment(new Date(2019, 7, 29, 12)),
-      end_time: moment(new Date(2019, 8, 2, 22)),
-    },
-    {
-      id: 5,
-      group: 5,
-      title: '',
-      type: 'task',
-      start_time: moment(new Date(2019, 8, 3)),
-      end_time: moment(new Date(2019, 8, 7, 5)),
-    },
-    {
-      id: 6,
-      group: 6,
-      title: '',
-      type: 'task',
-      start_time: moment(new Date(2019, 8, 6, 2)),
-      end_time: moment(new Date(2019, 8, 10)),
-    },
-  ]
-
-  return (
-    <div style={{height: '100%'}}>
-      <GridContainer style={{height: '100%'}}>
-        <GridItem xs={12} sm={12} md={7}>
-          <Table
-            tableHeaderColor="default"
-            tableHead={["TYPE", "ID", "SUBJECT", "STATUS", "ASSIGNEE"]}
-            tableData={[
-              ["PHASE", "42", "Construction", "", ""],
-              ["LEVEL", "43", "Level 1", "", ""],
-              ["AREA", "44", "Area A", "", ""],
-              ["TASK", "45", "Excavation", "Started", "Jim's Digging"],
-              ["TASK", "46", "Underground", "On Deck", "PP Plumbers"],
-              ["TASK", "47", "Form Foundation", "Not Started", "Rick's Concrete"],
-              ["TASK", "48", "Pour Foundation", "Not Started", "Rick's Concrete"],
-              ["TASK", "49", "Strip Forms", "Not Started", "Rick's Concrete"],
-              ["AREA", "50", "Area B", "", ""],
-              ["TASK", "51", "Underground", "On Deck", "Carpenters"],
-              ["TASK", "52", "Form Foundation", "Not Started", "Carpenters"],
-              ["TASK", "53", "Pour Foundation", "Not Started", "Carpenters"],
-              ["TASK", "54", "Strip Forms", "Not Started", "Carpenters"],
-            ]}
-          />
-          <div>
-            <CustomButtons className={classes.newBtn}>+ Create new work</CustomButtons>
-            <p className={classes.txt}>(1-1/1)</p>
-          </div>
-        </GridItem>
-        <GridItem xs={12} sm={12} md={5}>
-            <div className={classes.topButtonWrapper}>
-              <CustomButtons className={classNames([classes.btn, classes.createBtn])}>
-                + Create
-                <ExpandMoreIcon style={{margin: 0}} />
-              </CustomButtons>
-              <CustomButtons className={classNames([classes.btn, classes.createBtn])}>
-                Show CPM
-              </CustomButtons>
-              <CustomButtons className={classes.btn}>
-                <LocalBarIcon style={{color: '#fa8f35', width: 12, height: 12}} />
-                <span style={{padding: '0 3px'}}>Filter</span>
-                <span className={classes.circle}>1</span>
-              </CustomButtons>
-              <CustomButtons className={classes.btn}>
-                <span className={classes.circle}>i</span>
-              </CustomButtons>
-              {
-                icons.map((item, key) => {
-                  return (
-                    <CustomButtons className={classes.btn} key={key}>
-                      <item.icon className={classes.btnIcon}/>
-                    </CustomButtons>
-                  )
-                })
-              }
-            </div>
-            <CustomTimelineChart
-              groups={groups}
-              items={items}
-              sidebarWidth={0}
-              itemHeightRatio={0.17}
-              defaultTimeStart={moment(new Date(2019, 7, 29))}
-              defaultTimeEnd={moment(new Date(2019, 8, 10))}
+  render() {
+    const icons = [
+      { icon: CenterFocusWeakIcon },
+      { icon: ZoomOutIcon },
+      { icon: ZoomInIcon },
+      { icon: SaveOutlinedIcon },
+      { icon: ZoomOutMapIcon },
+      { icon: MoreVertOutlinedIcon }
+    ]
+    
+    const groups = [
+      { id: 1, title: '' },
+      { id: 2, title: '' },
+      { id: 3, title: '' },
+      { id: 4, title: '' },
+      { id: 5, title: '' },
+      { id: 6, title: '' },
+      { id: 7, title: '' },
+      { id: 8, title: '' },
+      { id: 9, title: '' },
+      { id: 10, title: '' },
+      { id: 11, title: '' },
+      { id: 12, title: '' },
+      { id: 13, title: '' }
+    ]
+    
+    const items = [
+      {
+        id: 1,
+        group: 1,
+        title: '',
+        type: 'phase',
+        start_time: moment(new Date(2019, 7, 29, 12)),
+        end_time: moment(new Date(2019, 8, 10)),
+      },
+      {
+        id: 2,
+        group: 2,
+        title: '',
+        type: 'level',
+        start_time: moment(new Date(2019, 7, 29, 12)),
+        end_time: moment(new Date(2019, 8, 10)),
+      },
+      {
+        id: 3,
+        group: 3,
+        title: '',
+        type: 'area',
+        start_time: moment(new Date(2019, 7, 29, 12)),
+        end_time: moment(new Date(2019, 8, 10)),
+      },
+      {
+        id: 4,
+        group: 4,
+        title: '',
+        type: 'task',
+        start_time: moment(new Date(2019, 7, 29, 12)),
+        end_time: moment(new Date(2019, 8, 2, 22)),
+      },
+      {
+        id: 5,
+        group: 5,
+        title: '',
+        type: 'task',
+        start_time: moment(new Date(2019, 8, 3)),
+        end_time: moment(new Date(2019, 8, 7, 5)),
+      },
+      {
+        id: 6,
+        group: 6,
+        title: '',
+        type: 'task',
+        start_time: moment(new Date(2019, 8, 6, 2)),
+        end_time: moment(new Date(2019, 8, 10)),
+      },
+    ]
+    return (
+      <div style={{height: '100%'}}>
+        <GridContainer style={{height: '100%'}}>
+          <GridItem xs={12} sm={12} md={7}>
+            <Table
+              tableHeaderColor="default"
+              tableHead={["TYPE", "ID", "SUBJECT", "STATUS", "ASSIGNEE"]}
+              tableData={[
+                ["PHASE", "42", "Construction", "", ""],
+                ["LEVEL", "43", "Level 1", "", ""],
+                ["AREA", "44", "Area A", "", ""],
+                ["TASK", "45", "Excavation", "Started", "Jim's Digging"],
+                ["TASK", "46", "Underground", "On Deck", "PP Plumbers"],
+                ["TASK", "47", "Form Foundation", "Not Started", "Rick's Concrete"],
+                ["TASK", "48", "Pour Foundation", "Not Started", "Rick's Concrete"],
+                ["TASK", "49", "Strip Forms", "Not Started", "Rick's Concrete"],
+                ["AREA", "50", "Area B", "", ""],
+                ["TASK", "51", "Underground", "On Deck", "Carpenters"],
+                ["TASK", "52", "Form Foundation", "Not Started", "Carpenters"],
+                ["TASK", "53", "Pour Foundation", "Not Started", "Carpenters"],
+                ["TASK", "54", "Strip Forms", "Not Started", "Carpenters"],
+              ]}
             />
-        </GridItem>
-      </GridContainer>
-    </div>
-  );
+            <div>
+              <CustomButtons className={styles.newBtn}>+ Create new work</CustomButtons>
+              <p className={styles.txt}>(1-1/1)</p>
+            </div>
+          </GridItem>
+          <GridItem xs={12} sm={12} md={5}>
+              <div className={styles.topButtonWrapper}>
+                <CustomButtons className={classNames([styles.btn, styles.createBtn])}>
+                  + Create
+                  <ExpandMoreIcon style={{margin: 0}} />
+                </CustomButtons>
+                <CustomButtons className={classNames([styles.btn, styles.createBtn])}>
+                  Show CPM
+                </CustomButtons>
+                <CustomButtons className={styles.btn}>
+                  <LocalBarIcon style={{color: '#fa8f35', width: 12, height: 12}} />
+                  <span style={{padding: '0 3px'}}>Filter</span>
+                  <span className={styles.circle}>1</span>
+                </CustomButtons>
+                <CustomButtons className={styles.btn}>
+                  <span className={styles.circle}>i</span>
+                </CustomButtons>
+                {
+                  icons.map((item, key) => {
+                    return (
+                      <CustomButtons className={styles.btn} key={key}>
+                        <item.icon className={styles.btnIcon}/>
+                      </CustomButtons>
+                    )
+                  })
+                }
+              </div>
+              <CustomTimelineChart
+                groups={groups}
+                items={items}
+                sidebarWidth={0}
+                itemHeightRatio={0.17}
+                defaultTimeStart={moment(new Date(2019, 7, 29))}
+                defaultTimeEnd={moment(new Date(2019, 8, 10))}
+              />
+          </GridItem>
+        </GridContainer>
+      </div>
+    );
+  }
 }
